7721246b140cd961dcb8eaa55c2523d1eab5bc90
Mohammad Echaary <mohammadechaary@gmail.com>
2024-07-26 06:02:37 +0100

    new commit

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -15,19 +15,15 @@ let quotes = [
   },
 ];
 
-let newQuoteText = document.getElementById("newQuoteText"),
-  newQuoteCategory = document.getElementById("newQuoteCategory");
-
 function showRandomQuote() {
   let randomNumber = Math.floor(quotes.length * Math.random());
   quoteDisplay.innerHTML = quotes[randomNumber].text;
 }
 showRandomQuote();
 
-function createAddQuoteForm(event) {
-  event.preventDefault();
-  let text = newQuoteText.value.trim(),
-    category = newQuoteCategory.value.trim();
+function addQuote() {
+  let text = inputNewQuote.value.trim(),
+    category = inputQuoteCate.value.trim();
   if (text !== "" && category !== "") {
     quotes.push({ text, category });
   }
@@ -35,3 +31,20 @@ function createAddQuoteForm(event) {
   newQuoteCategory.value = "";
   console.log(quotes);
 }
+
+let div = document.createElement("div");
+let inputNewQuote = document.createElement("input");
+inputNewQuote.id = "newQuoteText";
+inputNewQuote.type = "text";
+inputNewQuote.placeholder = "Enter a new quote";
+let inputQuoteCate = document.createElement("input");
+inputQuoteCate.id = "newQuoteCategory";
+inputQuoteCate.type = "text";
+inputQuoteCate.placeholder = "Enter quote category";
+let quotebtn = document.createElement("button");
+quotebtn.textContent = "Add Quote";
+div.appendChild(inputNewQuote);
+div.appendChild(inputQuoteCate);
+div.appendChild(quotebtn);
+document.body.appendChild(div);
+quotebtn.addEventListener("click", () => addQuote());
